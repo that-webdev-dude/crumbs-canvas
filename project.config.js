21f7eb03fb6b203feac1f5c7d6532f2416e6a07f
that-webdev-dude <that.webdev.dude@gmail.com>
2022-05-20 21:53:26 +0100

    first

---
+++ project.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'crumbs-canvas',
+  pages: [
+    // if making a multipage
+    // static site, add your pages here
+    {
+      title: 'index',
+      filename: 'index.html',
+      template: './src/views/index.html',
+    },
+  ],
+};
